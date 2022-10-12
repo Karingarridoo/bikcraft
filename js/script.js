7781f64f91312a246a76c91bcbc6ff72b680234f
Karin Garrido <kogkog@gmail.com>
2022-10-12 13:39:54 -0300

    js galeria imagens

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ const links = document.querySelectorAll(".header-menu a");
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
-  if (url.includes(href)){
+  if (url.includes(href)) {
     link.classList.add("ativo");
   }
 }
 
-links.forEach (ativarLink);
+links.forEach(ativarLink);
 
 //ativar itens do orçamento
 
@@ -25,23 +25,41 @@ parametros.forEach(ativarProduto);
 
 //perguntas frequentes
 
-const perguntas = document.querySelectorAll('.perguntas button');
+const perguntas = document.querySelectorAll(".perguntas button");
 
 function ativarPergunta(event) {
   const pergunta = event.currentTarget;
   const controls = pergunta.getAttribute("aria-controls");
   const resposta = document.getElementById(controls);
- 
+
   resposta.classList.toggle("ativa");
-  const ativa = resposta.classList.contains("ativa")
-  pergunta.setAttribute('aria-expanded', ativa);
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
   console.log(resposta);
 }
 
-
 function EventosPerguntas(pergunta) {
-  pergunta.addEventListener('click', ativarPergunta);
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
 perguntas.forEach(EventosPerguntas);
 
+// Galeria de imagens
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaConteiner = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  console.log(media);
+  if (media) {
+    galeriaConteiner.prepend(img);
+  }
+}
+
+function EventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(EventosGaleria);
